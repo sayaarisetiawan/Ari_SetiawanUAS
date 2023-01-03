8b0a04b19aa2f18123050bace30906fef741df94
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-01-03 19:57:59 +0700

    soal no 10

--- no10.php
+++ no10.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>soal no 9</title>
+    <title>soal no 10</title>
 </head>
 <body>
 <?php
 $a = 50;
 $b = 10;
-if ($a > $b) {
+if ($a == $b) {
     echo "Benar<br>";
-} 
+} else {
+    echo "Salah<br>";
+}
 ?>
 </body>
 </html>
